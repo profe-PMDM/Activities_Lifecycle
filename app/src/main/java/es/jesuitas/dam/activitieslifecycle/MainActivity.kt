0dd2b2a100e65590088d8bbb146e666a7e3b9a8a
David Corcuera <david.corcuera@gmail.com>
2021-10-27 20:03:59 +0200

    save and restore data on Activity terminate (onSaveInstanceState)

--- app/src/main/java/es/jesuitas/dam/activitieslifecycle/MainActivity.kt
+++ app/src/main/java/es/jesuitas/dam/activitieslifecycle/MainActivity.kt
@@ -8,13 +8,23 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.core.app.ShareCompat
+import es.jesuitas.dam.activitieslifecycle.databinding.ActivityMainBinding
+
+const val KEY_TEXT = "text_key"
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         Log.i("MainActivity", "onCreate Called")
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState != null) {
+            binding.editText.setText(savedInstanceState.getString(KEY_TEXT, "Nada"))
+        }
+
     }
 
     override fun onStart() {
@@ -37,7 +47,14 @@ class MainActivity : AppCompatActivity() {
         Log.i("MainActivity", "onStop Called")
     }
 
-    override fun onDestroy() {
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val text : String = binding.editText.text.toString()
+        outState.putString(KEY_TEXT, text)
+        Log.i("MainActivity","onSaveInstanceState Called")
+    }
+
+        override fun onDestroy() {
         super.onDestroy()
         Log.i("MainActivity", "onDestroy Called")
     }
